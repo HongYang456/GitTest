57cf03c38885d5de13518e2377fb22ce2673e4e8
HongYang456 <2395909838@qq.com>
2020-12-17 18:30:51 +0800

    MVP简单抽取3

---
+++ first1/src/main/java/com/example/first1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.first1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.tv);
+    }
+}
